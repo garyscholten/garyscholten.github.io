09069541b3a29a32f4818473e136cc19f1972692
garyscholten <gary@slashweb.ca>
2015-10-26 11:10:16 -0600

    Create rgbhistogram.cpp

---
+++ rgbhistogram.cpp
@@ -0,0 +1,59 @@
+#include "rgbhistogram.h"
+#include <QImage>
+#include <QClipboard>
+#include <QGuiApplication>
+
+RgbHistogram::RgbHistogram(QObject *parent) : QObject(parent),m_binCount(1),m_maxValue(1) { }
+
+void RgbHistogram::compute()
+{
+    QImage img(m_imageSource.toLocalFile());
+    if (img.isNull())
+        return;
+
+    m_bins.clear();
+    m_bins.resize(m_binCount);
+
+    for (int i=0; i<img.height(); ++i) {
+        const QRgb *pixels = reinterpret_cast<const QRgb *>(img.constScanLine(i));
+        for (int j=0; j<img.width(); ++j) {
+            m_bins[binForValue(qRed(*pixels))].red++;
+            m_bins[binForValue(qGreen(*pixels))].green++;
+            m_bins[binForValue(qBlue(*pixels))].blue++;
+            ++pixels;
+        }
+    }
+
+    m_maxValue = 0;
+    foreach (const RgbBin &bin, m_bins) {
+        if (bin.red > m_maxValue)
+            m_maxValue = bin.red;
+        if (bin.green > m_maxValue)
+            m_maxValue = bin.green;
+        if (bin.blue > m_maxValue)
+            m_maxValue = bin.blue;
+    }
+    emit maxValueChanged(m_maxValue);
+    emit histogramUpdated();
+}
+
+int RgbHistogram::redCount(int bin) const
+{
+    if (bin >= m_binCount)
+        return 0;
+    return m_bins[bin].red;
+}
+
+int RgbHistogram::greenCount(int bin) const
+{
+    if (bin >= m_binCount)
+        return 0;
+    return m_bins[bin].green;
+}
+
+int RgbHistogram::blueCount(int bin) const
+{
+    if (bin >= m_binCount)
+        return 0;
+    return m_bins[bin].blue;
+}
